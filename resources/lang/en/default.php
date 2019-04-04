4ae6d88fc9475ec3261d97a074e1e1cc9af83ae0
Daniele Barbaro <barbaro.daniele@gmail.com>
2019-04-04 09:50:21 +0200

    Add lang file

---
+++ resources/lang/en/default.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'type' => 'Type',
+    'email' => 'Email',
+    'notification_type' => 'Notification type',
+    'date' => 'Date',
+    'sns-list' => 'SNS List',
+
+];
