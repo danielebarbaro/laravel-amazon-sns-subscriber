7eaa0b29605b703e08259538ad7709fb3b46ae71
Daniele Barbaro <barbaro.daniele@gmail.com>
2019-04-04 09:50:55 +0200

    Add sns response table on index page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SnsResponse;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $sns_result = SnsResponse::where('notification_type', 'bounce')->paginate(50);
+        return view('sns-response.index', compact('sns_result'));
     }
 }
